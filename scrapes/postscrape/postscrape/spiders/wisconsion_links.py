461eabdc2bd87386486e3d8d4e9161ab0f31eb90
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2021-02-18 23:59:54 -0500

    links multiple update

--- scrapes/postscrape/postscrape/spiders/wisconsion_links.py
+++ scrapes/postscrape/postscrape/spiders/wisconsion_links.py
@@ -1,6 +1,8 @@
 import scrapy
 import os
 
+masterLinks = []
+
 
 class LinksSpider(scrapy.Spider):
     name = "wisconsin_links"
@@ -10,11 +12,21 @@ class LinksSpider(scrapy.Spider):
         'https://www.dhs.wisconsin.gov/news/2020.htm?items_per_page=All'
     ]
 
+    count = 0
+
     def parse(self, response):
+        self.count += 1
         links = response.css(
             '.field.field-name-title-field.field-type-text.field-label-hidden > .header-2 > a::attr(href)').getall()
-        os.chdir('./links/')
-        filename = 'all_wisconsin_links.txt'
-        with open(filename, 'w') as f:
-            f.write(','.join(links))
-        os.chdir('..')
+        masterLinks.extend(links)
+
+        if self.count == len(self.start_urls):
+            writeToFile(masterLinks)
+
+
+def writeToFile(Finallinks):
+    os.chdir('./links/')
+    filename = 'all_wisconsin_links.txt'
+    with open(filename, 'w') as f:
+        f.write(','.join(Finallinks))
+    os.chdir('..')
