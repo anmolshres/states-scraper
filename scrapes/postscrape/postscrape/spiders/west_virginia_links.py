54e485b52bca0c328380f3ffaa72d7bdd7ea4a0a
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2021-03-05 04:08:12 -0500

    links update to hold title

--- scrapes/postscrape/postscrape/spiders/west_virginia_links.py
+++ scrapes/postscrape/postscrape/spiders/west_virginia_links.py
@@ -11,10 +11,15 @@ class LinksSpider(scrapy.Spider):
 
     def parse(self, response):
         links = response.css('td > a::attr(href)').getall()
+        titles = response.css('td > a > strong::text').getall()
         cutoff_idx = links.index('/healthalerts/documents/wv/WVHAN_163.pdf')
         links = links[0:cutoff_idx]
+        titles = titles[0:cutoff_idx]
+        final_list = []
+        for idx in range(cutoff_idx):
+            final_list.append(links[idx]+';;'+titles[idx])
         os.chdir('./links/')
         filename = 'all_west_virginia_links.txt'
         with open(filename, 'w') as f:
-            f.write(','.join(links))
+            f.write(','.join(final_list))
         os.chdir('..')
